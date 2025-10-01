f2ee49572b2c997dce5b2e98b42218f3554bfc19
Damien <maitredede@users.noreply.github.com>
2025-10-01 16:26:55 +1100

    chore(deps): register tiff codec

--- examples/plutobook-basic/imagecodecs.go
+++ examples/plutobook-basic/imagecodecs.go
@@ -4,10 +4,11 @@ import (
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
+
 	// _ "github.com/samuel/go-pcx/pcx"
 	// _ "golang.org/x/image/bmp"
 	// _ "golang.org/x/image/riff"
-	// _ "golang.org/x/image/tiff"
+	_ "golang.org/x/image/tiff"
 	// _ "golang.org/x/image/vp8"
 	// _ "golang.org/x/image/vp8l"
 	// _ "golang.org/x/image/webp"
