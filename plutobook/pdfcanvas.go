77975c1943f4fd131db4978d0221bce4c050d844
Damien <maitredede@users.noreply.github.com>
2025-10-20 11:00:03 +1100

    feat(plutobook): pdf canvas create

--- plutobook/pdfcanvas.go
+++ plutobook/pdfcanvas.go
@@ -1,25 +1,44 @@
 package plutobook
 
+import (
+	"fmt"
+	"unsafe"
+
+	"github.com/jupiterrider/ffi"
+	"github.com/maitredede/puregolibs/strings"
+)
+
 type PDFCanvas struct {
 	CanvasBase
 }
 
 var _ canvasInterface = (*PDFCanvas)(nil)
 
-// func CreatePDFCanvas(filename string, size PageSize) (*PDFCanvas, error) {
-// 	libInit()
-// 	ptr := libPDFCanvasCreate(filename, size)
-// 	if ptr == 0 {
-// 		msg := libGetErrorMessage()
-// 		return nil, fmt.Errorf("pdf canvas create failed: %v", msg)
-// 	}
-// 	c := &PDFCanvas{
-// 		CanvasBase: CanvasBase{
-// 			ptr: ptr,
-// 		},
-// 	}
-// 	return c, nil
-// }
+func CreatePDFCanvas(filename string, size PageSize) (*PDFCanvas, error) {
+	libInit()
+
+	sym := mustGetSymbol("plutobook_pdf_canvas_create")
+
+	var cif ffi.Cif
+	if ok := ffi.PrepCif(&cif, ffi.DefaultAbi, 2, &ffi.TypePointer, &ffi.TypePointer, &ffiPageSizeType); ok != ffi.OK {
+		panic("plutobook_pdf_canvas_create cif prep is not OK")
+	}
+
+	cFilename := strings.CString(filename)
+	var ptr canvasPtr
+	ffi.Call(&cif, sym, unsafe.Pointer(&ptr), unsafe.Pointer(cFilename), unsafe.Pointer(&size))
+
+	if ptr == nil {
+		msg := libGetErrorMessage()
+		return nil, fmt.Errorf("pdf canvas create failed: %v", msg)
+	}
+	c := &PDFCanvas{
+		CanvasBase: CanvasBase{
+			ptr: ptr,
+		},
+	}
+	return c, nil
+}
 
 func (c *PDFCanvas) SetMetadata(metadata PdfMetadata, value string) error {
 	libInit()
@@ -30,14 +49,22 @@ func (c *PDFCanvas) SetMetadata(metadata PdfMetadata, value string) error {
 	return nil
 }
 
-// func (c *PDFCanvas) SetSize(size PageSize) error {
-// 	libInit()
-// 	if c.ptr == nil {
-// 		return ErrCanvasIsClosed
-// 	}
-// 	libPDFCanvasSetSize(c.ptr, size)
-// 	return nil
-// }
+func (c *PDFCanvas) SetSize(size PageSize) error {
+	libInit()
+	if c.ptr == nil {
+		return ErrCanvasIsClosed
+	}
+	// libPDFCanvasSetSize(c.ptr, size)
+	// return nil
+	sym := mustGetSymbol("plutobook_pdf_canvas_set_size")
+
+	var cif ffi.Cif
+	if ok := ffi.PrepCif(&cif, ffi.DefaultAbi, 2, &ffi.TypeVoid, &ffi.TypePointer, &ffiPageSizeType); ok != ffi.OK {
+		panic("plutobook_pdf_canvas_set_size cif prep is not OK")
+	}
+	ffi.Call(&cif, sym, nil, unsafe.Pointer(&c.ptr), unsafe.Pointer(&size))
+	return nil
+}
 
 func (c *PDFCanvas) ShowPage() error {
 	libInit()
